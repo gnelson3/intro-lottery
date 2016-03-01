3826fe79de21cf1a72b407e41f2698e48a46d362
gnelson3 <dnelsoncarter@aol.com>
2016-03-01 00:56:48 -0500

    lottery game

---
+++ lottery.js
@@ -0,0 +1,45 @@
+var moneyLeft = 1000001;
+var lottoCost = 100000;
+var lottoWins = 10000;
+
+
+document.getElementById("money-left").innerHTML = moneyLeft;
+
+function lotteryNumberPicker() {
+    var randNum = Math.random();
+    randNum = randNum * 10;
+    randNum = randNum + 1;
+    randNum = Math.floor(randNum);
+    document.getElementById("lotto-number").innerHTML = randNum ;
+    
+    return randNum;
+}
+
+function userNumberPicker() {
+    var userNum = prompt("What number would you like?");
+    document.getElementById("user-number").innerHTML = userNum;
+    return userNum;
+}
+
+function didUserWin(userNum, lottoNum) {
+    return userNum == lottoNum;
+}
+
+function payForTicket() {
+    moneyLeft = moneyLeft - lottoCost;
+    document.getElementById("money-left").innerHTML = moneyLeft;
+}
+
+function winner(){
+    moneyLeft = moneyLeft + lottoWins;
+    document.getElementById("money-left").innerHTML = moneyLeft
+}
+
+function lotteryGo() {
+    payForTicket();
+    var userNum = userNumberPicker();
+    var lottoNum = lotteryNumberPicker();
+    if (didUserWin(userNum, lottoNum)) {
+        
+    }
+}
